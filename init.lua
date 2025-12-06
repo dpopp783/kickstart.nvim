dd3eeac53df5d8b5c0dca02c323117063df56824
Daniel Popp <dpopp783@gmail.com>
2025-12-06 01:27:42 -0700

    add some key maps for vertical movement

--- init.lua
+++ init.lua
@@ -103,6 +103,10 @@ vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
+-- Vertical movement keymaps
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+
 -- Keybinds to make split navigation easier.
 -- Use CTRL+<hjkl> to switch between windows
 --
