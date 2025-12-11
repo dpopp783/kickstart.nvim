0e9d24294715a597700393fd34d0857ed41de67b
Daniel Popp <dpopp783@gmail.com>
2025-12-10 21:23:33 -0700

    install vim-be-good

--- init.lua
+++ init.lua
@@ -165,9 +165,14 @@ rtp:prepend(lazypath)
 --    :Lazy update
 --
 -- NOTE: Here is where you install your plugins.
+-- TODO: Install:
+-- harpoon
+-- undo tree
+-- tpope/vim-fugitive
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+  'ThePrimeagen/vim-be-good',
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
