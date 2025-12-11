a6d128ec58a1043a372e8772e1b9643b0ccefd97
Daniel Popp <dpopp783@gmail.com>
2025-12-11 03:27:53 -0700

    add more remaps

--- lua/daniel/remap.lua
+++ lua/daniel/remap.lua
@@ -6,6 +6,9 @@ vim.keymap.set('n', '<leader><leader>x', '<cmd>source %<CR>')
 vim.keymap.set('n', '<leader>x', ':.lua<CR>')
 vim.keymap.set('v', '<leader>x', ':lua<CR>')
 
+-- Quickly open file explorer
+vim.keymap.set('n', '<leader>n', vim.cmd.Ex, { desc = 'Open [n]etrw' })
+
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
@@ -27,17 +30,36 @@ vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
+-- Vertical movement keymaps
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', '<c-d>', '<c-d>zz')
--- Vertical movement keymaps
+
+-- Center search results
+vim.keymap.set('n', 'n', 'nzz')
+vim.keymap.set('n', 'N', 'Nzz')
 
 -- Move lines in Visual-Line mode, including proper indentation
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
+-- Keep cursor in place when appending lines with J
+vim.keymap.set('n', 'J', 'mzJ`z')
+
+-- Put without overwriting register
+vim.keymap.set('x', '<leader>p', '"_dP')
+
+-- Keep vim registers and system clipboard separate
+vim.keymap.set('n', '<leader>y', '"+y')
+vim.keymap.set('v', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>Y', '"+Y')
+
+-- Delete into void buffer
+vim.keymap.set('n', '<leader>d', '"_d')
+vim.keymap.set('v', '<leader>d', '"_d')
+
 -- Keybinds to make split navigation easier.
 -- Use CTRL+<hjkl> to switch between windows
---
+
 --  See `:help wincmd` for a list of all window commands
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
@@ -49,3 +71,6 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+
+vim.keymap.set('n', '<leader>S', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = '[S]ubstitute word' })
+-- TODO: Add shortcuts for navigiating quick fix list
